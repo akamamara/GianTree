5d8de04d1b1508ba2aa767a32a7f2e0193c367e4
akamamara <aldi_okta10@apps.ipb.ac.id>
2020-09-16 15:03:51 +0700

    Add navbar

---
+++ mobile-first/js/script.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var x = document.getElementById("nav-content");
+
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+
+    else {
+        x.style.display = "block";
+    }
+}
